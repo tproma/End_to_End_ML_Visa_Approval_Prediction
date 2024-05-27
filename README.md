9ef3b2b9c94ca673c4650d645d50e0666f44f0e1
Tanjina <tanjinaproma@gmail.com>
2024-05-27 22:48:23 +0600

    setup file added

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ conda create -p venv python=3.8 -y
 ```
 conda activate venv/
 ```
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa_prediction",
+    version="0.0.0",
+    author="tproma",
+    author_email="tanjinaproma@gmail.com",
+    packages=find_packages()
+)
