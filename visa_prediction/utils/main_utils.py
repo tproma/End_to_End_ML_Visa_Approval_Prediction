aa3cf7a3304c2db2a7b285a85113c8a016420808
Tanjina <tanjinaproma@gmail.com>
2024-06-02 09:56:22 +0600

    array saving method added

--- visa_prediction/utils/main_utils.py
+++ visa_prediction/utils/main_utils.py
@@ -46,3 +46,21 @@ def load_object(file_path: str) -> object:
 
     except Exception as e:
         raise USvisaException(e, sys) from e
+    
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+
+
+
