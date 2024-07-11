0cdc65142c219efb0c7305ebfd8123f79ad1d5d0
Tanjina <tanjinaproma@gmail.com>
2024-07-11 06:38:29 +0600

    Data Ingestion Related Constants added

--- visa_prediction/constants/__init__.py
+++ visa_prediction/constants/__init__.py
@@ -20,3 +20,14 @@ FILE_NAME: str = "us_visa.csv"
 TRAIN_FILE_NAME :str = "train.csv"
 TEST_FILE_NAME : str = "test.csv"
 
+
+"""
+Data Ingestion Related Constants 
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME :str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR :str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT: float = 0.2
+
+
