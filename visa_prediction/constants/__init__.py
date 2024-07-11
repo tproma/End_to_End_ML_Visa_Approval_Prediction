553505259dbef1cc23215fd1b2bfdf4948eb4e3a
Tanjina <tanjinaproma@gmail.com>
2024-07-11 06:31:46 +0600

    constants added

--- visa_prediction/constants/__init__.py
+++ visa_prediction/constants/__init__.py
@@ -5,3 +5,14 @@ DATABASE_NAME = "US_VISA"
 
 COLLECTION_NAME = "visa_data"
 MONGODB_URL_KEY = "MONGODB_URL"
+
+
+PIPELINE_NAME:str = "visa_prediction"
+ARTIFACT_DIR:str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
+
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
