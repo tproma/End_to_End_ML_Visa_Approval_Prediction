5d7db1b55c7ed10d197ddaac2f67322d4ba62b3d
Tanjina <tanjinaproma@gmail.com>
2024-07-11 06:59:34 +0600

    mongodb configuration file added

---
+++ visa_prediction/configuration/mongodb_connection.py
@@ -0,0 +1,8 @@
+import sys
+
+from visa_prediction.exception import USvisaException
+from visa_prediction.logger import logging
+
+import os
+from visa_prediction.constants import DATABASE_NAME, MONGODB_URL_KEY
+
