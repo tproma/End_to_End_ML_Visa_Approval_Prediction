c59e9dc603c7c000a5c5fd0c99e5215d9fe9fec9
Tanjina <tanjinaproma@gmail.com>
2024-08-15 10:34:39 +0600

    data access modified

--- visa_prediction/data_access/visa_prediction_data.py
+++ visa_prediction/data_access/visa_prediction_data.py
@@ -31,7 +31,6 @@ class USVisaData:
             df = pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df = df.drop(columns=["_id"], axis=1)
-
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
