e465807af888aac5a758e0996ecca6c96af1ebf0
Tanjina <tanjinaproma@gmail.com>
2024-08-15 21:09:00 +0600

    variable name  corrected

--- visa_prediction/entity/config_entity.py
+++ visa_prediction/entity/config_entity.py
@@ -20,6 +20,6 @@ class DataIngestionConfig:
     feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
     training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
     testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
-    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT
     collection_name:str = DATA_INGESTION_COLLECTION_NAME
 
