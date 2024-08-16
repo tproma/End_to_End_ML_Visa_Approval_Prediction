87d37380610c73786cdaf7d58e7c4ca72eba7408
Tanjina <tanjinaproma@gmail.com>
2024-08-16 23:57:51 +0600

    DataIngestion class constructed

--- visa_prediction/components/data_ingestion.py
+++ visa_prediction/components/data_ingestion.py
@@ -10,3 +10,13 @@ from visa_prediction.exception import USvisaException
 from visa_prediction.logger import logging
 from visa_prediction.data_access.visa_prediction_data import USVisaData
 
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig=DataIngestionConfig()):
+        """
+        :param data_ingestion_config: configuration for data ingestion
+        """
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise USvisaException(e,sys)
