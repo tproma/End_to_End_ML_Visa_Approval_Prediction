f11d3c268f7e869b5e8c3fd0cb8940fdb5c2fd5d
Tanjina <tanjinaproma@gmail.com>
2024-08-17 00:00:38 +0600

    export_data_into_feature_store added

--- visa_prediction/components/data_ingestion.py
+++ visa_prediction/components/data_ingestion.py
@@ -20,3 +20,21 @@ class DataIngestion:
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise USvisaException(e,sys)
+        
+
+    def export_data_into_feature_store(self)->DataFrame:
+        try:
+            logging.info(f"Exporting data from mongodb")
+            usvisa_data = USVisaData()
+            dataframe = usvisa_data.export_collection_as_dataframe(collection_name=
+                                                                   self.data_ingestion_config.collection_name)
+            logging.info(f"Shape of dataframe: {dataframe.shape}")
+            feature_store_file_path  = self.data_ingestion_config.feature_store_file_path
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            logging.info(f"Saving exported data into feature store file path: {feature_store_file_path}")
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
+
+        except Exception as e:
+            raise USvisaException(e,sys)
