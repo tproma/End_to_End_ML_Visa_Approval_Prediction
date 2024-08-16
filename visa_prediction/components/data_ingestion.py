0b24b20afd043a7b48932a103b4466cd83f3927b
Tanjina <tanjinaproma@gmail.com>
2024-08-17 00:06:48 +0600

    split_data_as_train_test added

--- visa_prediction/components/data_ingestion.py
+++ visa_prediction/components/data_ingestion.py
@@ -45,3 +45,25 @@ class DataIngestion:
 
         except Exception as e:
             raise USvisaException(e,sys)
+        
+    
+
+    def split_data_as_train_test(self,dataframe: DataFrame) ->None:
+        logging.info("Entered split_data_as_train_test method of Data_Ingestion class")
+
+        try:
+            train_set, test_set = train_test_split(dataframe, test_size=self.data_ingestion_config.train_test_split_ratio)
+            logging.info("Performed train test split on the dataframe")
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
+            logging.info(f"Exporting train and test file path.")
+            train_set.to_csv(self.data_ingestion_config.training_file_path,index=False,header=True)
+            test_set.to_csv(self.data_ingestion_config.testing_file_path,index=False,header=True)
+
+            logging.info(f"Exported train and test file path.")
+        except Exception as e:
+            raise USvisaException(e, sys) from e
