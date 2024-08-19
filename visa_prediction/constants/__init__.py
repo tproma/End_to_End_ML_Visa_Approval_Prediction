7bfdde55295e4e34c36a23849ad2c04a05b58eca
Tanjina <tanjinaproma@gmail.com>
2024-08-19 09:57:44 +0600

    Data Validation realted contant added

--- visa_prediction/constants/__init__.py
+++ visa_prediction/constants/__init__.py
@@ -31,3 +31,10 @@ DATA_INGESTION_INGESTED_DIR :str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
 
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
