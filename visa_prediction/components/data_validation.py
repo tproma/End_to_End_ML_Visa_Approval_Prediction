60e526cefaca9fe1ce4b66dc418b2d01e1828c20
Tanjina <tanjinaproma@gmail.com>
2024-08-20 23:37:35 +0600

    Data Validation component modified

--- visa_prediction/components/data_validation.py
+++ visa_prediction/components/data_validation.py
@@ -1,3 +1,4 @@
+import sys
 from pandas import DataFrame
 
 from visa_prediction.exception import USvisaException
@@ -6,5 +7,18 @@ from visa_prediction.utils.main_utils import read_yaml_file, write_yaml_file
 from visa_prediction.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from visa_prediction.entity.config_entity import DataValidationConfig
 
+from visa_prediction.constants import SCHEMA_FILE_PATH
 
 
+class DataValidation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_config: DataValidationConfig):
+        """
+        :param data_ingestion_artifact: Output reference of data ingestion artifact stage
+        :param data_validation_config: configuration for data validation
+        """
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config =read_yaml_file(file_path=SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise USvisaException(e,sys)
