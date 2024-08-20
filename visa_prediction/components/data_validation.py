7ea50eab4f088733342dd88b321118ad60306978
Tanjina <tanjinaproma@gmail.com>
2024-08-20 23:31:42 +0600

    Data Validation Component added

---
+++ visa_prediction/components/data_validation.py
@@ -0,0 +1,8 @@
+from pandas import DataFrame
+
+from visa_prediction.exception import USvisaException
+from visa_prediction.logger import logging
+from visa_prediction.utils.main_utils import read_yaml_file, write_yaml_file
+from visa_prediction.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from visa_prediction.entity.config_entity import DataValidationConfig
+
