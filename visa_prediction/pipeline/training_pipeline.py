522cbe35ed73085bfb64fedfabc61bd2ddc2be53
Tanjina <tanjinaproma@gmail.com>
2024-08-26 11:55:03 +0600

    start_data_validation method added

--- visa_prediction/pipeline/training_pipeline.py
+++ visa_prediction/pipeline/training_pipeline.py
@@ -1,9 +1,18 @@
 import sys
 
 from visa_prediction.components.data_ingestion import DataIngestion
+from visa_prediction.components.data_validation import DataValidation
 
-from visa_prediction.entity.config_entity import DataIngestionConfig
-from visa_prediction.entity.artifact_entity import DataIngestionArtifact
+
+
+from visa_prediction.entity.config_entity import (DataIngestionConfig,
+                                                DataValidationConfig)
+
+
+from visa_prediction.entity.artifact_entity import (DataIngestionArtifact,
+                                                    DataValidationArtifact)
+
+
 
 from visa_prediction.exception import USvisaException
 from visa_prediction.logger import logging
@@ -23,6 +32,21 @@ class TrainingPipeline:
             logging.info("Exit Data Ingestion method of Training Pipeline")
 
             return data_ingestion_artifact
+        except Exception as e:
+            raise USvisaException(e,sys) from e
+        
+    
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            logging.info("Entered the start_data_validation method of TrainPipeline class")
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact, 
+                                             data_validation_config= self.data_validation_config)
+            
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation operation")
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+            
+            return data_validation_artifact
         except Exception as e:
             raise USvisaException(e,sys) from e
         
