f0fef214b443c54239d152c5856b8468896e9afb
Tanjina <tanjinaproma@gmail.com>
2024-08-26 11:57:18 +0600

    data_validation calledc from run pipeline

--- visa_prediction/pipeline/training_pipeline.py
+++ visa_prediction/pipeline/training_pipeline.py
@@ -54,5 +54,6 @@ class TrainingPipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise USvisaException(e,sys)
