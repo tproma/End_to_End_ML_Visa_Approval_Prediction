b89aead6ee2df77d8be6c15d0e1e761264e49bf6
Tanjina <tanjinaproma@gmail.com>
2024-08-27 23:32:18 +0600

    MODEL TRAINER related constants added

--- visa_prediction/constants/__init__.py
+++ visa_prediction/constants/__init__.py
@@ -45,3 +45,13 @@ Data Transformation related constants
 DATA_TRANSFORMATION_DIR_NAME:str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformated"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformated_object"
+
+
+"""
+MODEL TRAINER related constant
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
