b3261002cc1680a290af5b57dc4abe2b7dbc8f38
Tanjina <tanjinaproma@gmail.com>
2024-08-27 10:06:08 +0600

    DataTransformationConfig Added

--- visa_prediction/constants/__init__.py
+++ visa_prediction/constants/__init__.py
@@ -43,5 +43,5 @@ DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 Data Transformation related constants
 """
 DATA_TRANSFORMATION_DIR_NAME:str = "data_transformation"
-DATA_TRANSFORMATION_TRANSFORMED_DIR:str = "transformated"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformated"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformated_object"
