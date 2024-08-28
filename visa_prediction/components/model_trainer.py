dd151bd78d0ee60eda7763512d68b2abb3c024aa
Tanjina <tanjinaproma@gmail.com>
2024-08-28 14:12:53 +0600

    model trainer component added

---
+++ visa_prediction/components/model_trainer.py
@@ -0,0 +1,24 @@
+import sys
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import (accuracy_score, 
+                             f1_score,
+                              precision_score, 
+                              recall_score)
+from neuro_mf import ModelFactory
+
+from visa_prediction.exception import USvisaException
+from visa_prediction.logger import logging
+from visa_prediction.utils.main_utils import (load_numpy_array_data,
+                                            read_yaml_file,
+                                            load_object,
+                                            save_object)
+from visa_prediction.entity.config_entity import ModelTrainerConfig
+from visa_prediction.entity.artifact_entity import (ModelTrainerArtifact,
+                                                    DataTransformationArtifact, 
+                                                    ClassificationMetricArtifact
+)
