b1fd4d5361e06a12a340de7b827a35c93adce5d6
Tanjina <tanjinaproma@gmail.com>
2024-08-28 13:49:46 +0600

    ClassificationMetricArtifact added

--- visa_prediction/entity/artifact_entity.py
+++ visa_prediction/entity/artifact_entity.py
@@ -21,5 +21,13 @@ class DataTransformationArtifact:
 
 
 @dataclass
+class ClassificationMetricArtifact:
+    f1_score : float
+    precision_score : float
+    recall_score : float
+
+
+@dataclass
 class ModelTrainerArtifact:
     trained_model_file_path : str
+    metric_artifact : ClassificationMetricArtifact
