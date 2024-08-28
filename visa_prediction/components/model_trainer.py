aa5d199fd4f277d54076ceadb4d6c3bde789a6bc
Tanjina <tanjinaproma@gmail.com>
2024-08-28 23:17:55 +0600

    get_model_object_and_report method added

--- visa_prediction/components/model_trainer.py
+++ visa_prediction/components/model_trainer.py
@@ -33,4 +33,32 @@ class ModelTrainer:
         self.model_trainer_config = model_trainer_config
 
 
+    def get_model_object_and_report(self, train: np.array, test: np.array) -> Tuple[object, object]:
+        try:
+            logging.info("Using neuro_mf to get best model object and report")
+            model_factory = ModelFactory(model_config_path=self.model_trainer_config.model_config_file_path)
+            x_train, y_train, x_test, y_test = train[:,:-1], train[:,-1], test[:,:-1], test[:,-1],
+            best_model_detail = model_factory.get_best_model(
+                X = x_train, 
+                y = y_train, 
+                base_accuracy= self.model_trainer_config.expected_accuracy
+            )
+            model_obj = best_model_detail.best_model
+
+            y_pred = model_obj.predict(x_test)
+
+            accuracy = accuracy_score(y_test, y_pred) 
+            f1 = f1_score(y_test, y_pred)  
+            precision = precision_score(y_test, y_pred)  
+            recall = recall_score(y_test, y_pred)
+            metric_artifact = ClassificationMetricArtifact(
+                f1_score=f1, 
+                precision_score=precision, 
+                recall_score=recall)
+            
+            return best_model_detail, metric_artifact
+
+        
+        except Exception as e:
+            raise USvisaException(e,sys) from e
    
