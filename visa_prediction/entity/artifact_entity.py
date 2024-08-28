c644499b44ee6bb9bb2eee5211601526843e0898
Tanjina <tanjinaproma@gmail.com>
2024-08-28 13:48:08 +0600

    ModelTrainerArtifact added

--- visa_prediction/entity/artifact_entity.py
+++ visa_prediction/entity/artifact_entity.py
@@ -18,3 +18,8 @@ class DataTransformationArtifact:
     transformed_object_file_path: str 
     transformed_train_file_path: str
     transformed_test_file_path: str
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path : str
