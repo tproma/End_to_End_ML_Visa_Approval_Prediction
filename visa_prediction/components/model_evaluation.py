d406f8bf77af267483dbbaa6937a37a576d1e0ab
Tanjina <tanjinaproma@gmail.com>
2024-09-03 10:10:53 +0600

    get_best_model added

--- visa_prediction/components/model_evaluation.py
+++ visa_prediction/components/model_evaluation.py
@@ -7,7 +7,7 @@ from visa_prediction.logger import logging
 import sys
 import pandas as pd
 from typing import Optional
-from visa_prediction.entity.estimator import USvisaEstimator
+from visa_prediction.entity.s3_estimator import USVisaEstimator
 from dataclasses import dataclass
 from visa_prediction.entity.estimator import USvisaModel
 from visa_prediction.entity.estimator import TargetValueMapping
@@ -31,3 +31,29 @@ class ModelEvaluation:
             self.model_trainer_artifact = model_trainer_artifact
         except Exception as e:
             raise USvisaException(e, sys) from e
+        
+
+
+    def get_best_model(self) -> Optional[USVisaEstimator]:
+        """
+        Method Name :   get_best_model
+        Description :   This function is used to get model in production
+        
+        Output      :   Returns model object if available in s3 storage
+        On Failure  :   Write an exception log and then raise an exception
+        """
+        try:
+            bucket_name = self.model_eval_config.bucket_name
+            model_path=self.model_eval_config.s3_model_key_path
+            usvisa_estimator = USVisaEstimator(bucket_name=bucket_name,
+                                               model_path=model_path)
+
+            if usvisa_estimator.is_model_present(model_path=model_path):
+                return usvisa_estimator
+            return None
+        except Exception as e:
+            raise  USvisaException(e,sys)
+        
+
+
+    
