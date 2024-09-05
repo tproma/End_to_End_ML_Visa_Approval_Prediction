d5cd93bf5ef46c323a15388f985fa0dcbcc10db2
Tanjina <tanjinaproma@gmail.com>
2024-09-05 10:46:33 +0600

    evaluate_model method added

--- visa_prediction/components/model_evaluation.py
+++ visa_prediction/components/model_evaluation.py
@@ -48,3 +48,36 @@ class ModelEvaluation:
         except Exception as e:
             raise USvisaException(e,sys)
         
+
+
+    def evaluate_model(self) -> EvaluateModelResponse:
+        try:
+            test_df = self.data_ingestion_artifact.test_file_path
+            test_df['company_age'] = CURRENT_YEAR- test_df['yr_of_estab']
+
+            x,y = test_df.drop(TARGET_COLUMN, axis = 1), test_df[TARGET_COLUMN]
+            y = y.replace(
+                TargetValueMapping()._asdict()
+            )
+
+            trained_model_f1_score = self.model_trainer_artifact.metric_artifact.f1_score
+
+            best_model_f1_score = None
+            best_model = self.get_best_model()
+            if best_model is not None:
+                y_hat_best_model = best_model.predict(x)
+                best_model_f1_score = f1_score(y,y_hat_best_model)
+
+
+            tmp_best_model_score = 0 if best_model_f1_score is None else best_model_f1_score
+            result = EvaluateModelResponse(trained_model_f1_score=trained_model_f1_score,
+                                           best_model_f1_score=best_model_f1_score,
+                                           is_model_accepted=trained_model_f1_score>tmp_best_model_score,
+                                           difference=trained_model_f1_score-tmp_best_model_score
+                                           )
+            
+            logging.info(f"Result: {result}")
+            return result
+                
+        except Exception as e:
+            raise USvisaException(e,sys)
