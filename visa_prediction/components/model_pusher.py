a093f2d2a496b43266fbdb7ed71838598e7924f1
Tanjina <tanjinaproma@gmail.com>
2024-09-07 23:30:12 +0600

    initiate_model_pusher added

--- visa_prediction/components/model_pusher.py
+++ visa_prediction/components/model_pusher.py
@@ -16,4 +16,20 @@ class ModelPusher:
          self.usvisa_estimator = USVisaEstimator(bucket_name=model_pusher_config.bucket_name,
                                                  model_path=model_pusher_config.s3_model_key_path)
          
-         
+
+    def initiate_model_pusher(self) -> ModelPusherArtifact:
+         logging.info("Entered initiate_model_pusher method of ModelTrainer class")   
+         try:
+            logging.info("Uploading artifacts folder to s3 bucket")
+            self.usvisa_estimator.save_model(from_file=self.model_evaluation_artifact.trained_model_path)
+
+            model_pusher_artifact = ModelPusherArtifact(bucket_name=self.model_pusher_config.bucket_name,
+                                                        s3_model_path=self.model_pusher_config.s3_model_key_path)
+            
+            logging.info("Uploaded artifacts folder to s3 bucket")
+            logging.info(f"Model pusher artifact: [{model_pusher_artifact}]")
+            logging.info("Exited initiate_model_pusher method of ModelTrainer class")
+            
+            return model_pusher_artifact
+         except Exception as e:
+              raise USvisaException(e,sys) from e
