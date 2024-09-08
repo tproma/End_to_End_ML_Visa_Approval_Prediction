63ce247016e32e962fbf546857f6660135de4479
Tanjina <tanjinaproma@gmail.com>
2024-09-08 22:40:45 +0600

    USVisaPredictorConfig added

--- visa_prediction/entity/config_entity.py
+++ visa_prediction/entity/config_entity.py
@@ -66,4 +66,10 @@ class ModelEvaluationConfig:
 class ModelPusherConfig:
     bucket_name:str = MODEL_BUCKET_NAME
     s3_model_key_path: str = MODEL_FILE_NAME
+
+
+@dataclass
+class USVisaPredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
     
