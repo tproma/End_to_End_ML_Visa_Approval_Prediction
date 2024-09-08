96d1fe0b2fdb738a8667814e140fac6659fd5624
Tanjina <tanjinaproma@gmail.com>
2024-09-08 22:47:27 +0600

    USVisaData class added in prediction pipeline

--- visa_prediction/pipeline/prediction_pipeline.py
+++ visa_prediction/pipeline/prediction_pipeline.py
@@ -3,10 +3,38 @@ import sys
 import numpy as np
 import pandas as pd
 
-from visa_prediction.entity.config_entity import USvisaPredictorConfig
+from visa_prediction.entity.config_entity import USVisaPredictorConfig
 from visa_prediction.entity.s3_estimator import USVisaEstimator
 from visa_prediction.exception import USvisaException
 from visa_prediction.logger import logging
 from visa_prediction.utils.main_utils import read_yaml_file
 from pandas import DataFrame
+
+
+class USVisaData:
+    def __init__(self, 
+                 continent,
+                 education_of_employee,
+                 has_job_experience,
+                 requires_job_training,
+                 no_of_employees,
+                 region_of_employment,
+                 prevailing_wage,
+                 unit_of_wage,
+                 full_time_position,
+                 company_age) :
+        try:
+            self.continent = continent
+            self.education_of_employee = education_of_employee
+            self.has_job_experience = has_job_experience
+            self.requires_job_training = requires_job_training
+            self.no_of_employees = no_of_employees
+            self.region_of_employment = region_of_employment
+            self.prevailing_wage = prevailing_wage
+            self.unit_of_wage = unit_of_wage
+            self.full_time_position = full_time_position
+            self.company_age = company_age
+
+        except Exception as e:
+            raise USvisaException(e,sys) from e
 
