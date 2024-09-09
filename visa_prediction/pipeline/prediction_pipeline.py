f2b3b17db098aa9cb2d352eb914c5cf28639685d
Tanjina <tanjinaproma@gmail.com>
2024-09-09 19:15:55 +0600

    get_usvisa_data_as_dict added

--- visa_prediction/pipeline/prediction_pipeline.py
+++ visa_prediction/pipeline/prediction_pipeline.py
@@ -51,3 +51,34 @@ class USVisaData:
         
         except Exception as e:
             raise USvisaException(e, sys) from e
+
+    
+
+    def get_usvisa_data_as_dict(self):
+        """
+        This function returns a dictionary from USvisaData class input 
+        """
+        logging.info("Entered get_usvisa_data_as_dict method as USvisaData class")
+
+        try:
+            input_data = {
+                "continent": [self.continent],
+                "education_of_employee": [self.education_of_employee],
+                "has_job_experience": [self.has_job_experience],
+                "requires_job_training": [self.requires_job_training],
+                "no_of_employees": [self.no_of_employees],
+                "region_of_employment": [self.region_of_employment],
+                "prevailing_wage": [self.prevailing_wage],
+                "unit_of_wage": [self.unit_of_wage],
+                "full_time_position": [self.full_time_position],
+                "company_age": [self.company_age],
+            }
+
+            logging.info("Created usvisa data dict")
+
+            logging.info("Exited get_usvisa_data_as_dict method as USvisaData class")
+
+            return input_data
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
